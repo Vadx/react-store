f1f1ea887349c46d6a3f03557313e3a368880bac
Vadx <lv_km@ukr.net>
2023-03-27 19:52:14 +0300

    upd d

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
-const { protocol, host } = window.location;
+// const { protocol, host } = window.location;
 
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
-    : (`${protocol}//${host}` as string);
+    : "https://json-server-vadx.vercel.app/";
+// : (`${protocol}//${host}` as string);
